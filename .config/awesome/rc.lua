9b723552aedb57611f35d6da9746c21b6baa694b
gpike75@gmail.com <gpike@isecpartners.com>
2013-06-22 17:35:15 -0700

    adding blingbling

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -42,8 +42,9 @@ modkey = "Mod4"
 run_once("nm-applet")
 run_once("gnome-sound-applet")
 run_once("udisks-glue")
-run_once("xautolock",  "-time 1 -locker 'gnome-screensaver-command --lock'")
-awful.util.spawn_with_shell("xmodmap " .. home .. "/.speedswapper")
+run_once("xautolock",  "-time 5 -locker 'gnome-screensaver-command --lock'")
+run_once("xmodmap ", home .. "/.speedswapper")
+--awful.util.spawn_with_shell("xmodmap " .. home .. "/.speedswapper")
 ---- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
 {
